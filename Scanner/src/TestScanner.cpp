a5c4ed5ff3aa3e7a95d4d9b724074df1fb541acf
Nick Nowak <nick.nowak92@yahoo.com>
2017-07-27 17:50:08 +0200

    Konsolen-Ausgabe uebersichtlicher gemacht

--- Scanner/src/TestScanner.cpp
+++ Scanner/src/TestScanner.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
 
-	Buffer*  buffer = new Buffer( (char*) "/home/nefl1011/workspace/SysProg/Buffer/src/TestText01.txt");
+	Buffer*  buffer = new Buffer( (char*) "/home/nick/workspace/SysProg/Buffer/src/TestText01.txt");
 	Automat* automat = new Automat();
 
 	Scanner* scanner = new Scanner(buffer, automat);
@@ -14,19 +14,19 @@ int main(int argc, char **argv) {
 		token = scanner->nextToken();
 
 		if (token->getType() == ERROR) {
-			cout << "Error: Das Zeichen \'" << token->getLexem() << "\' wird nicht akzeptiert!" << endl;
+			cout << "Error: Das Zeichen \'" << token->getLexem() << "\' wird nicht akzeptiert! \n" << endl;
 		}
 		else if (token->getType() == IDENTIFIER) {
-			cout << "Token " << token->getTypeName() << " Line: " << token->getLine()
-							<< " Spalte: " << token->getColumn() << " Lexem: " << token->getLexem() << endl;
+			cout << "Token " << token->getTypeName() << "\nLine: " << token->getLine()
+							<< "\t Spalte: " << token->getColumn() << "\t Lexem: " << token->getLexem() << "\n" << endl;
 		}
 		else if (token->getType() == INTEGER) {
-			cout << "Token " << token->getTypeName() << " Line: " << token->getLine()
-							<< " Spalte: " << token->getColumn() << " Value: " << token->getValue() << endl;
+			cout << "Token " << token->getTypeName() << "\nLine: " << token->getLine()
+							<< "\t Spalte: " << token->getColumn() << "\t Value: " << token->getValue() << "\n" << endl;
 		}
 		else {
-			cout << "Token " << token->getTypeName() << " Line: " << token->getLine()
-							<< " Spalte: " << token->getColumn() << " Sign: " << token->getLexem() << endl;
+			cout << "Token " << token->getTypeName() << "\nLine: " << token->getLine()
+							<< "\t Spalte: " << token->getColumn() << "\t Sign: " << token->getLexem() << "\n" << endl;
 		}
 	}
 
